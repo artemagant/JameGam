3255bc651af34e105d552196fe503fc154a64302
artemagant <al4166964@gmail.com>
2026-03-22 22:39:51 +0400

    Start working on settings

---
+++ jame-gam/Scenes/Settings_Menu/settings_menu.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+func _on_fs_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
